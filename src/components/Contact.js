aff7d3857aa8bc1712cefc16784847f323bbc35a
Naveen-Chhipa <naveenchhipa2003@gmail.com>
2025-03-21 14:52:10 +0530

    changes in contact us page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,22 @@
-const Contact=()=>{
-    return(
-        <div>
-            <h1>Contact us</h1>
-        </div>
-    )
-}
+const Contact = () => {
+  return (
+    <div>
+      <h1 className="font-bold m-2 p-2 text-3xl">Contact us</h1>
+      <form>
+        <input
+          type="text"
+          className="= p-2 m-2 border border-black rounded-lg"
+          placeholder="name"
+        ></input>
+        <input
+          className="p-2 m-2 border border-black rounded-lg"
+          placeholder="msg"
+        ></input>
+        <button className=" border border-black p-2 m-2 bg-gray-100 rounded-lg">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 export default Contact;
